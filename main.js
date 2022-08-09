0a9da23abba259f9fc439a2c93f5890709128b93
M4gnus99 <beaterquinteros@gmail.com>
2022-08-09 19:06:28 -0300

    desafio callback

---
+++ main.js
@@ -0,0 +1,13 @@
+let gastos =[15,200,54,123,764];
+
+function gastoConIva(num){
+    return num + (num * 0,21);
+}
+
+function funcionMap (num){
+    let aux=[];
+    aux = num.map(function gastoConIva(element){
+        return element + (element * 0,21);
+    })
+    return aux;
+}
