30c415af43ad4109fe83afed607bf83ff0914a0c
M4gnus99 <beaterquinteros@gmail.com>
2022-08-16 17:27:25 -0300

    modulo 18

--- modulo17/main.js
+++ modulo17/main.js
@@ -4,10 +4,11 @@ function gastoConIva(num){
     return num + (num * 0,21);
 }
 
-function funcionMap (num){
+function funcionMap (num,callback){
     let aux=[];
     aux = num.map(function gastoConIva(element){
         return element + (element * 0,21);
     })
+    
     return aux;
 }
